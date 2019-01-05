650777094dc1bed08273644c6468509881a84e66
Winston Hartnett <winstonhartnett@gmail.com>
2019-01-05 13:24:58 -0600

    Test Commit

---
+++ robot.py
@@ -0,0 +1,34 @@
+import commands
+import logging
+
+import wpilib
+from commandbased import CommandBasedRobot
+from wpilib.command import Scheduler
+
+
+class Marvin(CommandBasedRobot):
+
+    def robotInit(self):
+        pass
+
+    def disabledInit(self):
+        pass
+
+    def disabledPeriodic(self):
+        pass
+
+    def autonomousInit(self):
+        pass
+
+    def autonomousPeriodic(self):
+        pass
+
+    def teleopInit(self):
+        pass
+
+    def teleopPeriodic(self):
+        pass
+
+
+if __name__ == '__main__':
+    wpilib.run(Marvin)
